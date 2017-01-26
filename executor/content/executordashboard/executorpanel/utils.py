fb79632d5b59115bc53538b95ee0ab46c72ebe57
Alessandro De Carli <decarli.ale@gmail.com>
2017-01-26 11:29:24 +0100

    - on the way to mitaka support

--- executor/content/executordashboard/executorpanel/utils.py
+++ executor/content/executordashboard/executorpanel/utils.py
@@ -1,13 +1,15 @@
 import os
-
+from django.conf import settings
 from gc3libs.backends.openstack import OpenStackLrms, OpenStackVMPool
 
-from openstack_dashboard.api.nova import CACERT, INSECURE, nova_client
+from openstack_dashboard.api.nova import nova_client
 
 
 def inject_nova_client_auth_params(auth_params):
     # Monkey Patching for authentication
     oldOpenstackInit = OpenStackLrms.__init__
+    insecure = getattr(settings, 'OPENSTACK_SSL_NO_VERIFY', False)
+    cacert = getattr(settings, 'OPENSTACK_SSL_CACERT', None)
 
     username, token_id, project_id, nova_url, auth_url = auth_params
     c = nova_client.Client('1.1',
@@ -15,8 +17,8 @@ def inject_nova_client_auth_params(auth_params):
                            token_id,
                            project_id=project_id,
                            auth_url=auth_url,
-                           insecure=INSECURE,
-                           cacert=CACERT)
+                           insecure=insecure,
+                           cacert=cacert)
     c.client.auth_token = token_id
     c.client.management_url = nova_url
 
