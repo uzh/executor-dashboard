996d2cc07173909b234ff1c0ede07791f24b9fc1
Alessandro De Carli <decarli.ale@gmail.com>
2017-01-26 09:48:25 +0100

    config for uzh infrastructure

--- executor/local_settings.d/_9900_executor_settings.docker.py
+++ executor/local_settings.d/_9900_executor_settings.docker.py
@@ -9,3 +9,11 @@ OUTPUT_BASE_PATH = "/home/docker/persistent/gc3output"
 IGNORE_PARAMS = ['-s', '-l', '-o', '-r', '-v', '-u', '-l', '-N', '-C']
 
 OS_AUTH_URL="https://cloud.s3it.uzh.ch:5000/v2.0"
+
+
+OPENSTACK_HOST = "cloud.s3it.uzh.ch"
+OPENSTACK_KEYSTONE_URL = "https://%s:5000/v2.0" % OPENSTACK_HOST
+OPENSTACK_KEYSTONE_DEFAULT_ROLE = "_member_"
+
+ALLOWED_HOSTS = '*'
+USE_SSL = False
--- executor/local_settings.d/_9900_executor_settings.py
+++ executor/local_settings.d/_9900_executor_settings.py
@@ -9,3 +9,12 @@ IGNORE_PARAMS = ['-s', '-l', '-o', '-r', '-v', '-u', '-l', '-N', '-C']
 GC3PIE_CONF = "~/.gc3/gc3pie.conf"
 
 OS_AUTH_URL="https://cloud.s3it.uzh.ch:5000/v2.0"
+
+
+
+OPENSTACK_HOST = "cloud.s3it.uzh.ch"
+OPENSTACK_KEYSTONE_URL = "https://%s:5000/v2.0" % OPENSTACK_HOST
+OPENSTACK_KEYSTONE_DEFAULT_ROLE = "_member_"
+
+ALLOWED_HOSTS = '*'
+USE_SSL = False
